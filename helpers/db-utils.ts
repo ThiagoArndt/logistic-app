a53be2a7739a74e83a708e0733563a99cbb8eae0
Thiago Cirne Arndt <thiagoarndt04@gmail.com>
2023-04-09 20:07:17 -0300

    added env
    Signed-off-by: Thiago Cirne Arndt <thiagoarndt04@gmail.com>

--- helpers/db-utils.ts
+++ helpers/db-utils.ts
@@ -4,7 +4,7 @@ const con = createPool({
   connectTimeout: 10,
   host: "localhost",
   user: "root",
-  password: "123456",
+  password: process.env.DATABASE_PASSWORD,
   database: "almoxarifadodb",
 });
 
